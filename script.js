eec45cfccb7690667ebe700f2c8ffd486f740322
rfloresram <74856999+rfloresram@users.noreply.github.com>
2021-03-17 19:40:11 +0100

    Adding P Element

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-//You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+
+    const allEpisodes = getAllEpisodes();
+    makePageForEpisodes(allEpisodes);    
 }
+
+// function header(){
+//   const rootElem = getElementById("root");
+//   rootElem.innerHTML = 
+
+
+  
+// }
 
 function makePageForEpisodes(episodeList) {
-   const rootElem = document.getElementById("root");
+    
+  const rootElem = document.getElementById("root");
+    
+    episodeList.forEach(episodes => {
 
-    const tittleElement = document.createElement("h4")
-    const imageElement = document.createElement("img")
-    const pElement = document.createElement("p")
+        const divElement = document.createElement("div");
+        const tittleElement = document.createElement("h4");
+        const imageElement = document.createElement("img");
+        const summaryElement = document.createElement("div");
 
-    rootElem.append(tittleElement, imageElement, pElement)
-    console.log(rootElem)
+        //const pElement = document.createElement("p");
 
-    const title = document.querySelector("h4")
-    const image = document.querySelector("img")
-    const paragraph = document.querySelector("p")
-   
-    episodeList.forEach(episodes =>{
-    rootElem.innerHTML += title.innerText = episodes.name
-  });
+        divElement.append(tittleElement, imageElement, summaryElement);
+        
+        tittleElement.innerText = episodes.name;
+        imageElement.src = episodes.image.medium 
+        summaryElement.innerHTML = episodes.summary
+        //pElement.innerText = episodes.summary
+
+        rootElem.appendChild(divElement)
+    });
 }
-
 window.onload = setup;
 
+//Ctrl+K+U
