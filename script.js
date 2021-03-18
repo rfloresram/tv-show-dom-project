a8cf78b480fbae0deaed32730b0805ea3a6ca95e
rfloresram <74856999+rfloresram@users.noreply.github.com>
2021-03-18 17:55:44 +0100

    Season Number

--- script.js
+++ script.js
@@ -12,27 +12,50 @@ function setup() {
   
 // }
 
+function getSeasonNumber(seasonNumber){
+    if(seasonNumber <= 9 ){
+      return "0" + seasonNumber 
+    } 
+      return seasonNumber
+}      
+
 function makePageForEpisodes(episodeList) {
     
   const rootElem = document.getElementById("root");
+
+        let divElement = document.createElement("div");
+        let tittleElement = document.createElement("h4");
+        let imageElement = document.createElement("img");
+        let summaryElement = document.createElement("div");
+        let pElement = document.createElement("p");
+        let seasonElement =document.createElement("span")
+        let numberElement = document.createElement("span")
     
     episodeList.forEach(episodes => {
 
-        const divElement = document.createElement("div");
-        const tittleElement = document.createElement("h4");
-        const imageElement = document.createElement("img");
-        const summaryElement = document.createElement("div");
+         divElement = document.createElement("div");
+         tittleElement = document.createElement("h4");
+         imageElement = document.createElement("img");
+         summaryElement = document.createElement("div");
+         pElement = document.createElement("p");
+         seasonElement =document.createElement("span")
+         numberElement = document.createElement("span")
+         headerElement = document.createElement("div");
 
-        //const pElement = document.createElement("p");
+        headerElement.append(tittleElement,seasonElement,numberElement)
+  
+        divElement.append(headerElement, imageElement, summaryElement);
 
-        divElement.append(tittleElement, imageElement, summaryElement);
-        
-        tittleElement.innerText = episodes.name;
+        tittleElement.innerText = episodes.name
         imageElement.src = episodes.image.medium 
         summaryElement.innerHTML = episodes.summary
-        //pElement.innerText = episodes.summary
+        pElement.innerText = episodes.summary
+        seasonElement.innerText = "S" + getSeasonNumber(episodes.season)
+        numberElement.innerText = "E"+ episodes.number 
+
 
         rootElem.appendChild(divElement)
+
     });
 }
 window.onload = setup;
